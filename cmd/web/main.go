5af99001ef38303e77b24562fa40b1c26c6a0e43
Lukasz <taralalukasz123@gmail.com>
2023-12-05 11:27:42 +0100

    enabled TLS for our mux server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	_ "github.com/go-sql-driver/mysql"
@@ -81,11 +82,32 @@ func main() {
 	//configure ROUTES here
 	mux := app.routes()
 
+	//initialize  custom TLS configuration struct
+	tlsConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
+		//CipherSuites: []uint16{
+		//	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		//	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		//	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		//},
+		//MinVersion: tls.VersionTLS12,
+		//MaxVersion: tls.VersionTLS12,
+	}
+
 	//Create own server which uses the same logger to print logs
 	srv := &http.Server{
-		ErrorLog: errorLog,
-		Addr:     *addr,
-		Handler:  mux,
+		TLSConfig: tlsConfig,
+		ErrorLog:  errorLog,
+		Addr:      *addr,
+		Handler:   mux,
+		// Add Idle, Read and Write timeouts to the server.
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %v", *addr)
