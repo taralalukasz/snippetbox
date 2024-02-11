261f4d8a9d50ab93dc04bc763f52a29ca5af90d7
taralalukasz <taralalukasz123@gmail.com>
2024-02-11 17:40:09 +0100

    Chapter 2.6

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +20,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a snippet"))
+	
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Displaying snippet with id %v", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
