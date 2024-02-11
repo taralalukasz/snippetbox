53ee9d750ee366f0e677cfa65bc62446e279002b
taralalukasz <taralalukasz123@gmail.com>
2024-02-11 13:49:46 +0100

    Chapter 2.4

--- main.go
+++ main.go
@@ -6,13 +6,31 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if (r.URL.Path != "/") {
+		//this is an equivalent of 
+		// w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		// w.Header().Set("X-Content-Type-Options", "nosniff")
+		// w.WriteHeader(code)
+		http.NotFound(w, r)  
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create new snippet"))
 }
 
 func main() {
 	//create mux server and register function to corresponding endpoints
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	//this is how you start a web server
 	log.Println("Starting server on :4000")
